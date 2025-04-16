d8c2922dfe9a7a1166e1aee1f97119db4ad12c34
saiprakash-software <saiprakash24v@gmail.com>
2025-04-16 11:20:08 +0530

    initial

---
+++ shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
+
